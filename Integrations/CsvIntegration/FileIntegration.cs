359f76d678aeaef802876f668602b5c5c172fbb7
Björn <git@singali.net>
2020-04-20 00:34:05 +0200

    Crweates FileIntegration It reads from a IFileReader and returns those stories (minus the parents in the flattened hierarchy)

--- Integrations/CsvIntegration/FileIntegration.cs
+++ Integrations/CsvIntegration/FileIntegration.cs
@@ -1,6 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace StoryBuckets.Integrations.CsvIntegration
@@ -9,13 +8,26 @@ namespace StoryBuckets.Integrations.CsvIntegration
 	{
 		private readonly IFileReader _filereader;
 
-		public FileIntegration(IFileReader fileReader)
+		public FileIntegration(IFileReader filereader)
 		{
-			_filereader = fileReader;
+			_filereader = filereader;
 		}
 		public async Task<IEnumerable<IStoryFromIntegration>> FetchAsync()
 		{
-			throw new NotImplementedException();
+			var mappedStories = new List<IStoryFromIntegration>();
+			var parentIds = new HashSet<int>();
+			await foreach (var storyFromFile in _filereader.ParseAsync())
+			{
+				mappedStories.Add(new StoryFromIntegration { 
+					Id = storyFromFile.Id,
+					Title = storyFromFile.Title
+				});
+				
+				if(storyFromFile.ParentId.HasValue && !parentIds.Contains(storyFromFile.ParentId.Value))
+					parentIds.Add(storyFromFile.ParentId.Value);
+			}
+
+			return mappedStories.Where(story => !parentIds.Contains(story.Id));
 		}
 	}
 }
--- StoryBuckets.Integrations.Tests/CsvIntegration/FileIntegrationTests.cs
+++ StoryBuckets.Integrations.Tests/CsvIntegration/FileIntegrationTests.cs
@@ -1,23 +1,140 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using StoryBuckets.Integrations.CsvIntegration;
-using System;
+using Moq;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace StoryBuckets.Integrations.CsvIntegration.Tests
 {
     [TestClass()]
     public class FileIntegrationTests
     {
-        //[TestMethod()]
-        //public void Reads_stories_from_the_file()
-        //{
-        //    //Arrange
+        [TestMethod()]
+        public void Reads_stories_from_the_file()
+        {
+            //Arrange
+            var filereader = new FileREaderMock();
+            var integration = new FileIntegration(filereader.Object);
 
-        //    //Act
+            //Act
+            _ = integration.FetchAsync().Result;
 
-        //    //Assert
-        //    Assert.Fail();
-        //}
+            //Assert
+            filereader.Verify(mock => mock.ParseAsync(), Times.Once);
+        }
+
+        [TestMethod()]
+        public void Returns_the_stories_read_from_file()
+        {
+            //Arrange
+            var itemsToReturn = new[]
+            {
+                new FlattenedHierarchyItem(),
+                new FlattenedHierarchyItem(),
+                new FlattenedHierarchyItem(),
+                new FlattenedHierarchyItem()
+            };
+            var filereader = new FileREaderMock(itemsToReturn);
+            var integration = new FileIntegration(filereader.Object);
+
+            //Act
+            var result = integration.FetchAsync().Result;
+
+            //Assert
+            Assert.AreEqual(itemsToReturn.Count(), result.Count());
+            foreach (var item in result)
+            {
+                Assert.IsNotNull(item);
+            }
+        }
+
+        [TestMethod()]
+        public void Maps_Id()
+        {
+            //Arrange
+            var id = 42;
+            var itemsToReturn = new[]
+            {
+                new FlattenedHierarchyItem{
+                    Id = id
+                }
+            };
+            var filereader = new FileREaderMock(itemsToReturn);
+            var integration = new FileIntegration(filereader.Object);
+
+            //Act
+            var result = integration.FetchAsync().Result;
+
+            //Assert
+            Assert.AreEqual(id, result.Single().Id);
+        }
+
+        [TestMethod()]
+        public void Maps_Title()
+        {
+            //Arrange
+            var title = "Foobar deluxe";
+            var itemsToReturn = new[]
+            {
+                new FlattenedHierarchyItem{
+                    Title = title
+                }
+            };
+            var filereader = new FileREaderMock(itemsToReturn);
+            var integration = new FileIntegration(filereader.Object);
+
+            //Act
+            var result = integration.FetchAsync().Result;
+
+            //Assert
+            Assert.AreEqual(title, result.Single().Title);
+        }
+
+        [TestMethod()]
+        public void Do_not_include_parents_as_separate_items()
+        {
+            //Arrange
+            var id = 42;
+            var parentId = 2718;
+            var itemsToReturn = new[]
+            {
+                new FlattenedHierarchyItem{
+                    Id = id,
+                    ParentId = parentId
+                },
+                new FlattenedHierarchyItem
+                {
+                    Id = parentId
+                }
+            };
+            var filereader = new FileREaderMock(itemsToReturn);
+            var integration = new FileIntegration(filereader.Object);
+
+            //Act
+            var result = integration.FetchAsync().Result;
+
+            //Assert
+            Assert.AreEqual(id, result.Single().Id);
+        }
+
+        private class FileREaderMock : Mock<IFileReader>
+        {
+            public FileREaderMock(IEnumerable<FlattenedHierarchyItem> itemsToReturn = null) : base()
+            {
+                if (itemsToReturn == null)
+                    itemsToReturn = Enumerable.Empty<FlattenedHierarchyItem>();
+
+                Setup(fake => fake.ParseAsync()).Returns(MakeAsync(itemsToReturn));
+            }
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+            private async IAsyncEnumerable<T> MakeAsync<T>(IEnumerable<T> items)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+            {
+                foreach (var item in items)
+                {
+                    yield return item;
+                }
+            }
+        }
     }
 }
